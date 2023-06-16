dde5a71236170efad2399cd3622c12ec897f0dec
Abdulhamid254 <nassurabdulhamid49@gmail.com>
2023-06-16 04:02:56 -0700

    Craeted a cart model with 2 interfaces: cart interface & cartItem interface

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,16 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Cart } from 'src/app/models/cart.model';
 
 @Component({
   selector: 'app-cart',
-  template: `
-    <p>
-      cart works!
-    </p>
-  `,
-  styles: [
-  ]
+  templateUrl: `./cart.component.html`
+  
 })
 export class CartComponent implements OnInit {
+  cart: Cart = {items: []}
 
   constructor() { }
 
