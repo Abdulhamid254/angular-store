2788296ec3e675db477c92476ad7db06aeb0e1e9
Abdulhamid254 <nassurabdulhamid49@gmail.com>
2023-06-16 00:53:53 -0700

    Added mat-menus in the product-header-component & using output event emmiter shared data from product-header-component to the home component

--- src/app/pages/home/home-components/products-header/products-header.component.ts
+++ src/app/pages/home/home-components/products-header/products-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-products-header',
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   
 })
 export class ProductsHeaderComponent implements OnInit {
+  // output is an event emmiter that helps us send data out of our components to the parent component
+  @Output() columnsCountChange = new EventEmitter<number>();
 
+   sort = 'desc';
+   itemsShowCount = 12;
+   // sort variable by default having the property desending as the default
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  // method for updating the sort
+  onSortUpdated(newSort: string):void {
+       this.sort = newSort;
+  }
+
+  onItemsUpdated(count: number): void {
+    this.itemsShowCount = count;
+  }
+  //mehod for updating our layout
+  onColumnsUpdated(colsNum: number): void {
+    this.columnsCountChange.emit(colsNum);
   }
 
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
  
 })
 export class HomeComponent implements OnInit {
+  cols = 3;
+
+  //we are able to use the colsNum here because we used the output event emmitter to share data from the products-header to the this home component
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onColumnsCountChange(colsNum: number){
+    this.cols = colsNum;
   }
 
 }
