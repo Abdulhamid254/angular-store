dc1c5546d6600381402fe18729d719ce825bd27b
Abdulhamid254 <nassurabdulhamid49@gmail.com>
2023-06-16 06:34:19 -0700

    Added an event emitter on the onAddToCart method to helps us communicate between our product box and home component

--- src/app/pages/home/home-components/product-box/product-box.component.ts
+++ src/app/pages/home/home-components/product-box/product-box.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter} from '@angular/core',
+import { Product } from 'src/app/models/product.model';
 
 @Component({
   selector: 'app-product-box',
@@ -9,10 +10,27 @@ import { Component, Input, OnInit } from '@angular/core';
 export class ProductBoxComponent implements OnInit {
  // full widthmode here is we want when we show only one item per row that it should take the maximum available space
   @Input() fullWidthMode = false;
+  product: Product | undefined = {
+      id: 1,
+    title: 'snickers',
+    price: 150,
+    category: 'Shoes',
+    description: 'Description',
+    image: 'https://via.placeholder.com/150',
+  }
+
+  //oUTput helps us add event emitter for communication purposes between the child & parent component...like for us to be able to use this addtocart method in the home component
+  @Output() addToCart = new EventEmitter();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
+  // the on addtocart method is going to emit the product we get from the @input
+
+  onAddToCart(): void {
+     this.addToCart.emit(this.product);
+  }
 }
