438162ba86be476c632846489d6da4b67007fab3
Abdulhamid254 <nassurabdulhamid49@gmail.com>
2023-06-16 03:47:35 -0700

    Added conditional styling using [ngClass] to conditionaly render styles when we are in full width mode in the productbox component

--- src/app/pages/home/home-components/product-box/product-box.component.ts
+++ src/app/pages/home/home-components/product-box/product-box.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product-box',
-  templateUrl: `product-box.component.html`,
+  templateUrl: `./product-box.component.html`,
   styles: [
   ]
 })
 export class ProductBoxComponent implements OnInit {
+ // full widthmode here is we want when we show only one item per row that it should take the maximum available space
+  @Input() fullWidthMode = false;
 
   constructor() { }
 
