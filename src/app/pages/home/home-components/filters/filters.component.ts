ebdbc1a3a0e8f21da8860e14dd5ee8ef82c8596b
Abdulhamid254 <nassurabdulhamid49@gmail.com>
2023-06-19 07:31:20 -0700

    Subscribe to our endpoint getAllCategories in our home component

--- src/app/pages/home/home-components/filters/filters.component.ts
+++ src/app/pages/home/home-components/filters/filters.component.ts
@@ -1,24 +1,37 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { StoreService } from 'src/app/services/store.service';
 
 @Component({
   selector: 'app-filters',
   templateUrl: `./filters.component.html`,
   
 })
-export class FiltersComponent implements OnInit {
+export class FiltersComponent implements OnInit, OnDestroy {
     // output is an event emmiter that helps us send data out of our components to the parent component
 
   @Output() showCategory = new EventEmitter<string>();
-
-
-  categories = ['shoes','clarks','brocks']
-  constructor() { }
+  
+  categoriesSubscription: Subscription | undefined;
+  categories: Array<string> | undefined;
+  // categories = ['shoes','clarks','brocks'] initially we had hardcoded the value
+  constructor( private storeService: StoreService) { }
 
   ngOnInit(): void {
+   this.categoriesSubscription = this.storeService.getAllCategories()
+    .subscribe((res) => {
+      this.categories = res;
+    })
   }
 
   onShowCategory(category: string): void {
     this.showCategory.emit(category);
+  }
+
+  ngOnDestroy(): void {
+    if (this.categoriesSubscription) {
+      this.categoriesSubscription.unsubscribe();
+    }
   }
 
 }
