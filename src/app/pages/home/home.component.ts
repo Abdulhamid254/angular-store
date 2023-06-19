7f83cfb1dcf8022c639096446be896720387e5f7
Abdulhamid254 <nassurabdulhamid49@gmail.com>
2023-06-19 07:38:30 -0700

    Added the category parameter to the get products endpoint

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -46,7 +46,7 @@ export class HomeComponent implements OnInit, OnDestroy {
   }
 
   getProducts(): void {
-    this.productsSubscription = this.storeService.getAllProducts(this.count, this.sort)
+    this.productsSubscription = this.storeService.getAllProducts(this.count, this.sort,this.category)
     .subscribe((_products) =>{
       this.products = _products;
       // the _ infront of product is for making it private;
@@ -61,6 +61,7 @@ export class HomeComponent implements OnInit, OnDestroy {
 
   onShowCategory(newCategory: string):void{
       this.category = newCategory;
+      this.getProducts();
   }
 
   onAddToCart(product: Product):void{
