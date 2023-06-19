65b623ef974f8d0e5761cbcf4466515843b02a38
Abdulhamid254 <nassurabdulhamid49@gmail.com>
2023-06-19 10:49:16 -0700

    Added the functionality to store cartitems to localstorage even on refresh the state is preserved

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -3,36 +3,44 @@ import { BehaviorSubject } from 'rxjs';
 import { Cart, CartItem } from '../models/cart.model';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
-
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
-  // behavioursubject which allows components to subscribe and receive updates whenever the cart's value changes
-  cart = new BehaviorSubject<Cart>({items: []})
+  private cartKey = 'cart'; // Key used to store cart items in local storage
+    // behavioursubject which allows components to subscribe and receive updates whenever the cart's value changes
+  cart = new BehaviorSubject<Cart>({ items: [] });
 
-  constructor( private _snackBar: MatSnackBar) { }
-
+  constructor(private _snackBar: MatSnackBar) {
+    // Retrieve cart items from local storage on service initialization
+    const storedCart = localStorage.getItem(this.cartKey);
+    if (storedCart) {
+      this.cart.next(JSON.parse(storedCart));
+    }
+  }
 
   addToCart(item: CartItem): void {
     // accessing the beahvioursubject with the value
     // new array items created by spreading current items in cart
     const items = [...this.cart.value.items];
-
     const itemInCart = items.find((_item) => _item.id === item.id);
 
     if (itemInCart) {
-      itemInCart.quantity +=1;
-    }else {
+      itemInCart.quantity += 1;
+    } else {
       items.push(item);
     }
-    // emmiting this value so that any of the component that is subscribed to the cart can catch the value
-    this.cart.next({items});
-    this._snackBar.open('1 item added to cart.','Ok', {duration:3000});
-    // console.log(this.cart.value);
-    
+
+       // emmiting this value so that any of the component that is subscribed to the cart can catch the value
+    this.cart.next({ items });
+    this._snackBar.open('1 item added to cart.', 'Ok', { duration: 3000 });
+        // console.log(this.cart.value);
+
+    // Save cart items to local storage
+    localStorage.setItem(this.cartKey, JSON.stringify(this.cart.value));
   }
-removeQuantity(item: CartItem): void {
+
+  removeQuantity(item: CartItem): void {
     let itemForRemoval!: CartItem;
 
     let filteredItems = this.cart.value.items.map((_item) => {
@@ -42,43 +50,48 @@ removeQuantity(item: CartItem): void {
           itemForRemoval = _item;
         }
       }
-
       return _item;
     });
-      if (itemForRemoval) {
-        filteredItems = this.removeFromCart(itemForRemoval, false);
+
+    if (itemForRemoval) {
+      filteredItems = this.removeFromCart(itemForRemoval, false);
         // false here is sending a flag so that the user is not notified
-      }
-      this.cart.next({ items: filteredItems})
-      this._snackBar.open('1 item removed from cart.', 'Ok',{
-        duration: 3000
-      })
+    }
+
+    this.cart.next({ items: filteredItems });
+    this._snackBar.open('1 item removed from cart.', 'Ok', { duration: 3000 });
+
+    // Save cart items to local storage
+    localStorage.setItem(this.cartKey, JSON.stringify(this.cart.value));
   }
 
-  
-  getTotal(items: Array<CartItem>):number { 
-    return items.
-    map((item) => item.price * item.quantity)
-    .reduce((prev, current) => prev + current, 0);
-
+  getTotal(items: Array<CartItem>): number {
+    return items
+      .map((item) => item.price * item.quantity)
+      .reduce((prev, current) => prev + current, 0);
   }
+
   clearCart(): void {
     this.cart.next({ items: [] });
-    this._snackBar.open('Cart is cleared.', 'Ok', {
-      duration:3000
-    })
-  }
-  removeFromCart(item: CartItem,update= true):  Array<CartItem>{
-     const filteredItems = this.cart.value.items.filter(
-      (_item) => _item.id !== item.id
-     );
+    this._snackBar.open('Cart is cleared.', 'Ok', { duration: 3000 });
 
-     if (update) {
-        this.cart.next({ items: filteredItems});
-        this._snackBar.open('1 Item removed from cart.', 'Ok', {
-        duration:3000
-    })
-     }
-     return filteredItems;
-   }
+    // Remove cart items from local storage
+    localStorage.removeItem(this.cartKey);
+  }
+
+  removeFromCart(item: CartItem, update = true): Array<CartItem> {
+    const filteredItems = this.cart.value.items.filter(
+      (_item) => _item.id !== item.id
+    );
+
+    if (update) {
+      this.cart.next({ items: filteredItems });
+      this._snackBar.open('1 Item removed from cart.', 'Ok', { duration: 3000 });
+    }
+
+    // Save cart items to local storage
+    localStorage.setItem(this.cartKey, JSON.stringify(this.cart.value));
+
+    return filteredItems;
+  }
 }
